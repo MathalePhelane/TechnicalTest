991f8828b704b4d89f70509fcdadde61643f4b6f
MathalePhelane <phelane21@gmail.com>
2024-04-18 15:45:52 +0200

    added a methods inside the TextSplitter

--- TS.TechnicalTest/TextSplitter.cs
+++ TS.TechnicalTest/TextSplitter.cs
@@ -5,8 +5,14 @@ using System.Threading.Tasks;
 
 namespace TS.TechnicalTest
 {
-    public class TextSp[litter
+    public class TextSplitter : ITextSplitter
     {
+        public string[] SplitintoSentences(string text){
+            return text.Split(new[]{'.','?','!'}, StringSplitOptions.RemoveEmptyEntries);
+        }
+        public string[] SplitIntoWords(string sentence){
+            return sentence.Split(new[]{' '},StringSplitOptions.RemoveEmptyEntries);
+        }
         
     }
 }
