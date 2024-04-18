17f9e510ce50e426d07a376726a57bb8fa017306
Phelane <phelanem@legal.regn.net>
2024-04-18 17:23:49 +0200

    firast if stateent

--- TS.TechnicalTest/PitFinder.cs
+++ TS.TechnicalTest/PitFinder.cs
@@ -1,12 +1,27 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ExceptionServices;
 using System.Threading.Tasks;
 
 namespace TS.TechnicalTest
 {
-    public class PitFinder
+    public class PitFinder : IPitFinder
     {
+        public int FindDeepestPit (int[] A)
         
+        {
+              int deepestPt = -1;
+              int n = A.Length;
+
+              for(int i = 0; i < n -1 ; i++){
+                if(A[i] <= A[i +1]){
+                    continue;// this is mainly not a decreasing sequence so we can skipp it
+                }
+                
+
+              }
+
+        }
     }
 }
