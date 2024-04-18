41e41fbe4731b87ec93d9434f84e8dba633d0951
Phelane <phelanem@legal.regn.net>
2024-04-18 16:55:28 +0200

    added the main method and unit test

--- TS.UnitTests/1. LongestSentanceTests.cs
+++ TS.UnitTests/1. LongestSentanceTests.cs
@@ -1,6 +1,7 @@
 ﻿using TS.TechnicalTest;
 
 namespace TS.UnitTests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 [TestClass]
 public class LongestSentanceTests
@@ -8,9 +9,23 @@ public class LongestSentanceTests
     [TestMethod]
     public void Q1_BasicNominalTest()
     {
-        var input = $"We test coders. Give us a try";
-        var outcome = LongestSentanceAnswer.Solution(input);
+        var fakeTextSplitter = new TextSplitter();
+        var analyzer = new SentenceAnalyzer(fakeTextSplitter);
 
-        Assert.AreEqual(4, outcome);
+        var (sentence, wordCount) = analyzer.FindLongestSentence("We test coders. Give us a try?");
+
+        Assert.AreEqual("Give us a try", sentence.Trim());
+        Assert.AreEqual(4, wordCount);
+    }
+    [TestMethod]
+    public void Q2_BasicNominalTest()
+    {
+        var fakeTextSplitter = new TextSplitter();
+        var analyzer = new SentenceAnalyzer(fakeTextSplitter);
+
+        var (sentence, wordCount) = analyzer.FindLongestSentence("Forget CVs..Save time . x x");
+
+        Assert.AreEqual("Forget CVs", sentence.Trim());
+        Assert.AreEqual(2, wordCount);
     }
 }
