7a8fbcbbccb27445ca72cf0021a98c7b2d30eaa9
Phelane <phelanem@legal.regn.net>
2024-04-18 17:08:03 +0200

    created a new pit interface

---
+++ TS.TechnicalTest/IPitFinder.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TS.TechnicalTest
+{
+    public interface IPitFinder
+    {
+      int FindPit(int[] arr);   
+    }
+}
