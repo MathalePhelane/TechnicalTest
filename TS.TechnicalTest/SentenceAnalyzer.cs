be78076925381886531357b559d5405c5fc62e76
MathalePhelane <phelane21@gmail.com>
2024-04-18 16:07:17 +0200

    created a sentemce analyzer class for the logic

---
+++ TS.TechnicalTest/SentenceAnalyzer.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TS.TechnicalTest
+{
+    public class SentenceAnalyzer : ITextAnalyzer
+    {
+        private readonly ITextSplitter _textSplitter;
+
+        public SentenceAnalyzer(ITextSplitter textSpliter)
+        {
+            _textSplitter = textSpliter;
+        }
+    
+
+        public (string sentence,int wordCount) FindLongestSentence(string text)
+        {
+
+            var sentences = _textSplitter.SplitintoSentences(text);
+
+            string longestSentence = null;
+            int maxWordCount = 0;
+
+            foreach (var sentence in sentences)
+            {
+                var words = _textSplitter.SplitIntoWords(sentence);
+                var wordCount = words.Length;
+
+                if(wordCount > maxWordCount){
+                    maxWordCount = wordCount;
+                    longestSentence = sentence;
+                }
+            }
+
+            return(longestSentence,maxWordCount);
+        }
+
+    }
+}
